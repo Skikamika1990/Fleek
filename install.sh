b4a81cf06f4f67b41faa7e246b1938a554ccb418
Skikamika1990 <106681407+Skikamika1990@users.noreply.github.com>
2023-02-03 19:31:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install make build-essential libclang cmake protoc
+sudo apt install make build-essential libclang cmake protoc apt-transport-https ca-certificates curl software-properties-common
 curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
+
+wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+
 cd $HOME
-# качаем бінарник
+
 git clone https://github.com/fleek-network/ursa.git && cd ursa make install
 
 cd /etc/systemd/system
 [Unit]
 Description=My_Fleek
-
-# создать свой сервис
 
 tee <<EOF /etc/systemd/system/fleek.service
 
