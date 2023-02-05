d8e659f99bf1435bb11f5b36f53dab1d13199378
Skikamika1990 <106681407+Skikamika1990@users.noreply.github.com>
2023-02-05 18:56:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install make build-essential libclang cmake protoc apt-transport-https ca-certificates curl software-properties-common
 curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
 
